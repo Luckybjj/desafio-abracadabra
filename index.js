a491a47673e56d70ec1f6b6c2fd913ad19be8b1d
Bryan Lagos <lagos.bryan@gmail.com>
2022-04-18 23:55:19 -0400

    Abracadabra Run!

---
+++ index.js
@@ -0,0 +1,71 @@
+// Exporta Modulo , se crea una instancia express
+const express = require('express');
+// Se guarda en una constante app la ejecución de la instancia
+const app = express()
+// Puerto
+const PORT = 3000
+const fs = require('fs')
+//const path = require('path')
+
+// Método listen
+app.listen(PORT, () => {
+  console.log(`Server ON PORT ${PORT}`);
+})
+// Se ocupa un middleware y el método "static" de express para declarar la carpeta "assets" como directorio publico del servidor
+app.use(express.static("assets"));
+
+// Ruta raíz GET que devuelve el documento HTML
+/*
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+});
+*/
+// Ruta GET "/adacadabra/usuarios" que devuelve un archivo JSON
+app.get("/adacadabra/usuarios", (req, res) => {
+  res.sendFile(__dirname + '/usuarios.json')
+});
+
+// 05 ruta "/abracadabra/conejo/:n" que valide si el parametro n coincide con un número generado de forma aleatorea
+app.get("/abracadabra/conejo/:n", (req, res) => {
+  const num = Math.floor(Math.random() * (5 - 1)) + 1;
+  const n = req.params.n
+  /*
+  n == num
+      ? res.sendFile(__dirname + '/assets/conejito.jpg')
+      : res.sendFile(__dirname + '/assets/voldemort.jpg')
+    */
+  n == num
+    ? res.redirect('/conejito.jpg')
+    : res.redirect('/voldemort.jpg')
+});
+
+/*
+app.use("/assets", express.static(path.join(__dirname, "assets")));
+*/
+
+// Middleware con ruta "/abracadabra/juego/:usuario" para validar que el usuario recibido como parametro "usuario"
+// existe en el arreglo de nombres creado en el servidor.
+app.use('/abracadabra/juego/:nombre', (req, res, next) => {
+  const { nombre } = req.params;
+  console.log('nombre', nombre);
+
+  const users = JSON.parse(fs.readFileSync('./usuarios.json', 'utf-8'));
+  const lista = users.usuarios.filter((u) => u == nombre);
+
+  console.log('users', users);
+  console.log('lista', lista);
+
+  nombre == lista ? next() : res.redirect('/who.jpeg');
+});
+
+app.get('/abracadabra/juego/:nombre', (req, res, next) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Ruta generica que no esta definida en el servidor, que devuleva al mensaje "Esta página no exite.."
+// Ruta GET con paramatro "*"
+app.get("*", (req, res) => {
+  res.send(`<center><h1>Esta página no existe...</h1><center>
+    <center><h1 style="color:red">404 page not found</h1><center`)
+});
+
